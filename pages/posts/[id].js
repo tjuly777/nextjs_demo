8423a020df51ea3a51e633e75b43e64454353ca8
tjuly777 <tjuly777@gmail.com>
2021-12-01 19:49:27 +0900

    sytle:スタイル調整

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,8 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
-import { getAllPostIds, getPostData } from '../../lib/posts';
 import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.scss';
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export default function Post({ postData }) {
   return (
@@ -9,13 +10,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
     </Layout>
   );
 }
